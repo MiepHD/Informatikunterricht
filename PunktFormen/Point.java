ec48342fbee8795c4782bcfe630d775154ac65d1
MiepHD <63968466+MiepHD@users.noreply.github.com>
2023-04-24 20:05:04 +0200

    Added more casts to coordinateSystem

--- PunktFormen/Point.java
+++ PunktFormen/Point.java
@@ -5,14 +5,17 @@
  * @author Lia
  * @version (eine Versionsnummer oder ein Datum)
  */
-public class Point
-{
+public class Point {
     private double x;
     private double y;
     
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+    public Point(double[] array) {
+        this.x = array[0];
+        this.y = array[1];
     }
     public double getX() {
         return this.x;
--- PunktFormen/coordinateSystem.java
+++ PunktFormen/coordinateSystem.java
@@ -21,9 +21,38 @@ public class coordinateSystem
         return ((B[0] - A[0]) * (C[1] - A[1]) - (B[1] - A[1]) * (C[0] - A[0])) / 2;
     }
     public static Quadrilateral castToQuadrilateral(Rectangle rectangle) {
-        return new Quadrilateral(new Point(0, 0), new Point(rectangle.width, 0), new Point(rectangle.width, rectangle.height), new Point(0, rectangle.height));
+        return new Quadrilateral(
+            new Point(0, 0),
+            new Point(rectangle.width, 0),
+            new Point(rectangle.width, rectangle.height),
+            new Point(0, rectangle.height)
+        );
     }
+    
+    public static Quadrilateral castToQuadrilateral(ArrayQuadrilateral quadrilateral) {
+        return new Quadrilateral(
+            new Point(quadrilateral.A),
+            new Point(quadrilateral.B),
+            new Point(quadrilateral.C),
+            new Point(quadrilateral.D)
+        );
+    }
+    
     public static ArrayQuadrilateral castToArrayQuadrilateral(Rectangle rectangle) {
-        return new ArrayQuadrilateral(new double[] {0, 0}, new double[] {rectangle.width, 0}, new double[] {rectangle.width, rectangle.height}, new double[] {0, rectangle.height});
+        return new ArrayQuadrilateral(
+            new double[] {0, 0},
+            new double[] {rectangle.width, 0},
+            new double[] {rectangle.width, rectangle.height},
+            new double[] {0, rectangle.height}
+        );
+    }
+    
+    public static ArrayQuadrilateral castToArrayQuadrilateral(Quadrilateral quadrilateral) {
+        return new ArrayQuadrilateral(
+            new double[] {quadrilateral.A.getX(), quadrilateral.A.getY()},
+            new double[] {quadrilateral.B.getX(), quadrilateral.B.getY()},
+            new double[] {quadrilateral.C.getX(), quadrilateral.C.getY()},
+            new double[] {quadrilateral.D.getX(), quadrilateral.D.getY()}
+        );
     }
 }
